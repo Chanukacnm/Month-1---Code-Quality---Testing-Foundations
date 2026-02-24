39e37f704986015d6ccbaa69616f27bbaf0221e2
Chanuka Nimsara Mathagadeera <Chanukacnm98@gmail.com>
2026-02-24 15:25:38 +0530

    jest configuration and gitignore upload

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/components/**/*.{ts,tsx}',
+    '!src/components/**/*.test.{ts,tsx}',
+    '!src/components/**/index.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
